310e42fe142546ba19a1310e7b921dcf95a22dfc
Lee jaeyoung_Newbie <dldustn14@gmail.com>
2018-09-30 23:28:39 +0900

    test/file_input_output

--- myinbody/login.h
+++ myinbody/login.h
@@ -36,13 +36,17 @@ public:
 
 	void checkAccount(char *userID) {
 		FILE *check;
-
+		char a[15];
+		char b[15];
 		fopen_s(&check, "userList.txt", "r");
 
 		if (check == NULL)
 			printf("등록된 user 정보가 없습니다\n");
 
 		//userList 파일에서 매개변수로 받은 userID와 일치하는 항목이 있는지 확인하는 코드 작성 
+		//fprintf_s(check, "%s %s \n", userID, userPass);
+		fscanf_s(check, "%s %s \n", a,15, b,15); // fscanf_s 에서 문자열을 받을 경우 문자열의 최대 크기를 알려줘야한다.
+		printf("%s %s \n", a, b);
 		//exit(1);
 	}
 };
